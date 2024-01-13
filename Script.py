1814c5e387a8c3d6ae139db612e92a24255efaa6
Augusto Florentino <144605929+afiden@users.noreply.github.com>
2024-01-13 13:33:34 -0300

    Script de Geração

--- Script.py
+++ Script.py
@@ -10,21 +10,22 @@ import string
 # A senha é sempre igual, apenas dois espaços mudam
 # Você utilizará o script para gerar todas as possíveis combinações para este padrão
 # Para utilizar, modifique o base_password
+# Para modificar as senhas geradas, basta modificar os parâmetros "Digitos para a posição X" e "Digitos para a posição Y"
 
 # Base da senha
 base_password = "Exempl{}FN{}"
 
-# Possíveis caracteres para a posição X (letras e dígitos)
+# Digitos para a posição X
 characters_for_X = string.ascii_letters + string.digits
 
-# Possíveis dígitos para a posição Y (incluindo combinações de dois dígitos)
+# Dígitos para a posição Y
 digits_for_Y = [str(i) for i in range(100)]  # Gera números de 0 a 99
 
 # Gerar combinações
 passwords = [base_password.format(x, y) for x, y in itertools.product(characters_for_X, digits_for_Y)]
 
-# Exibir as primeiras 10 combinações para verificação
+# Exibir as primeiras 10 combinações
 print(passwords[:10])
 
-# Exibir o total de combinações geradas
+# Total de combinações geradas
 print(f"Total de combinações geradas: {len(passwords)}")
